f84804bb03b8ddf9486674819c30b28631a229a7
rishabh1347 <rishugupta0786@gmail.com>
2017-05-12 11:31:04 -0400

    somemessage

--- app/src/main/java/activity_main/netflix1/Main2Activity.java
+++ app/src/main/java/activity_main/netflix1/Main2Activity.java
@@ -5,19 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity {
     public void mainClick(View view) {
-        Intent intent = new Intent(this, Main2Activity.class);
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
-
-
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
     }
 }
-
